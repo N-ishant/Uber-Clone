acffcd67d5d5613ec1bab51d8a53925d150c07b4
Nishant Upreti <nishantupreti30@gmail.com>
2025-02-17 13:10:38 +0530

    Captain Home UI

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,6 +12,7 @@ import UserLogout from "./pages/UserLogout";
 import CaptainHome from "./pages/CaptainHome";
 import CaptainLogout from "./pages/CaptainLogout";
 import Riding from "./pages/Riding";
+import CaptainRiding from "./pages/CaptainRiding";
 
 const App = () => {
   return (
@@ -28,9 +29,10 @@ const App = () => {
         />
         <Route path="/signup" element={<UserSignup />} />
         <Route path="/login" element={<UserLogin />} />
-        <Route path="/riding" element={<Riding/>}/>
+        <Route path="/riding" element={<Riding />} />
         <Route path="/captain-signup" element={<CaptainSignup />} />
         <Route path="/captain-login" element={<CaptainLogin />} />
+        <Route path="/captain-riding" element={<CaptainRiding/>} />
         <Route
           path="/user/logout"
           element={
